70169abb74241f4cc9806148d97d3d75008b546c
John Lopez <john.lopez3695@gmail.com>
2024-03-16 14:41:25 -0400

    Added authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-// import * as controllers from "../controllers/auth.js";
+import * as controllers from "../controllers/auth.js";
 
 const router = Router();
 
@@ -7,9 +7,9 @@ const router = Router();
 router.post("/register", controllers.registerUser); //Will create a new user in the register endpoint
 router.post("/login", controllers.login); //Will log user in
 router.get("/verify", controllers.verify); //will verify user with their unique token
-router.post("/change-password", controllers.changePassword); //Will allow user to change their password
+// router.post("/change-password", controllers.changePassword); //Will allow user to change their password
 
 export default router;
 
 //ADMIN ROUTES
-// router.get("/users", controllers.getUsers);
+router.get("/users", controllers.getUsers);
