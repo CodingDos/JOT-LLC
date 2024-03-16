8884b7376939084c23890114817268677123c47f
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-16 12:18:32 -0400

    index.js file completed

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import authRoutes from "./authRoutes.js";
+// import categoryRoutes from "./category.js";  CREATE THIS FILE line 14
+// import counterRoutes from "./counter.js";   CREATE THIS FILE line 15
+
+const router = Router();
+
+router.get("/", (req, res) => res.send("This is the root")); //This is our landing page "Jot.com/"
+
+
+router.use("/auth", authRoutes); //authRoutes - will contain /auth/register, /auth/login
+
+
+router.use("/category", categoryRoutes);
+router.use("/counters", counterRoutes);
+
+export default router;
