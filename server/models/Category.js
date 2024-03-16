5a81d015681b49cfd9b8d58428fecbb886ba3f9d
John Lopez <john.lopez3695@gmail.com>
2024-03-16 19:23:26 -0400

    Modified Category schema to remove unique true from title

--- server/models/Category.js
+++ server/models/Category.js
@@ -5,11 +5,14 @@ const { Schema } = mongoose;
 let CategorySchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "users" }, //Every Category will reference the user id
-    title: { type: String, unique: true },
+    title: { type: String },
   },
   {
     timestamps: true, // This will add createdAt and updatedAt fields automatically
   }
 );
+
+// Create a compound index for userId and title to ensure uniqueness across this combination
+CategorySchema.index({ userId: 1, title: 1 }, { unique: true });
 
 export default mongoose.model("categories", CategorySchema);
