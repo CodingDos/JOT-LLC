f4fd1634eaba9ca68afbeebfcb5afc62469fa09a
John Lopez <john.lopez3695@gmail.com>
2024-03-16 15:15:52 -0400

    Auth debug complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,11 +7,9 @@ const router = Router();
 
 router.get("/", (req, res) => res.send("This is the root")); //This is our landing page "Jot.com/"
 
-
 router.use("/auth", authRoutes); //authRoutes - will contain /auth/register, /auth/login
 
-
-router.use("/category", categoryRoutes);
-router.use("/counters", counterRoutes);
+// router.use("/category", categoryRoutes);
+// router.use("/counters", counterRoutes);
 
 export default router;
