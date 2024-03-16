9875d6f832a64ac33cbf21dac9000aeb4be0a689
John Lopez <john.lopez3695@gmail.com>
2024-03-16 16:15:05 -0400

    Modified logic for category controllers to access headers token

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,48 +1,51 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import Category from "../models/Category.js";
 
-
 export const getCategories = async (req, res) => {
-    try {
-        const token = req.headers.authorization;
-        if (!token) {
-            return res.status(401).json({ error: "Authorization token not provided" });
-        }
-
-        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        const userId = decoded.id;
-
-        const categories = await Category.find({ userId });
-        res.json(categories);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Authorization token not provided" });
     }
-  };
 
-  export const createCategory = async (req, res) => {
-    try {
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).json({ error: "Authorization token not provided" });
-      }
-  
-      const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-      const userId = decoded.id;
-  
-      // Create a new category instance with user ID
-      const newCategory = new Category({ ...req.body, userId });
-  
-      // Save the new category to the database
-      await newCategory.save();
-  
-      res.status(201).json(newCategory);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const userId = decoded.id;
+
+    const categories = await Category.find({ userId });
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createCategory = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Authorization token not provided" });
     }
-  };
 
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const userId = decoded.id;
+
+    // Create a new category instance with user ID
+    const newCategory = new Category({ ...req.body, userId });
+
+    // Save the new category to the database
+    await newCategory.save();
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // router.get("/:id", controllers.getCategories); //Will get all categories based on userID
 // router.post("/create", controllers.createCategory);
--- server/routes/category.js
+++ server/routes/category.js
@@ -3,9 +3,9 @@ import * as controllers from "../controllers/category.js";
 
 const router = Router();
 
-router.get("/:id", controllers.getCategories); //Will get all categories based on userID
+router.get("/", controllers.getCategories); //Will get all categories based on userID
 router.post("/create", controllers.createCategory);
-router.put("/edit/:id", controllers.editCategory); // Will be able to edit a specific category using the category ID
-router.delete("/delete/:id", controllers.deleteCategory); // will be able to delete category using category ID
+// router.put("/edit/:id", controllers.editCategory); // Will be able to edit a specific category using the category ID
+// router.delete("/delete/:id", controllers.deleteCategory); // will be able to delete category using category ID
 
 export default router;
