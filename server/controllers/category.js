e6e1b766f6fb8cecb883d94def4c93c7e4d78aa8
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-16 15:25:40 -0400

    category controller - getCategories logic filled in using token info - test pls

---
+++ server/controllers/category.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import Category from "../models/Category.js";
+
+
+export const getCategories = async (req, res) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({ error: "Authorization token not provided" });
+        }
+
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        const userId = decoded.id;
+
+        const categories = await Category.find({ userId });
+        res.json(categories);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+  };
+
+
+
+
+
+// router.get("/:id", controllers.getCategories); //Will get all categories based on userID
+// router.post("/create", controllers.createCategory);
+// router.put("/edit/:id", controllers.editCategory); // Will be able to edit a specific category using the category ID
+// router.delete("/delete/:id", controllers.deleteCategory); // will be able to delete category using category ID
