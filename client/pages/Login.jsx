a4cd51ca871c5e60d2866763d9955d963317596e
John Lopez <john.lopez3695@gmail.com>
2024-03-17 14:57:47 -0400

    Modified form value to include deconstructed values

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,95 +1,89 @@
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 
-import { useState } from 'react'
-import { login } from '../services/users.js'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { login } from "../services/users.js";
+import { useNavigate } from "react-router-dom";
 
 function Login(props) {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [form, setForm] = useState({
-  username: '',
-  password: '',
-  isError: false,
-  errorMsg: '',
-  })
+    username: "",
+    password: "",
+    isError: false,
+    errorMsg: "",
+  });
 
   const handleChange = (event) => {
-  setForm({
-    ...form,
-    [event.target.name]: event.target.value,
-  })
-}
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const onSignIn = async (event) => {
-    event.preventDefault()
-    const { setUser } = props
+    event.preventDefault();
+    const { setUser } = props;
     try {
-      const user = await login(form)
-      setUser(user)
-      navigate('/category')
+      const user = await login(form);
+      setUser(user);
+      navigate("/category");
     } catch (error) {
-      console.error(error)
+      console.error(error);
       setForm({
         isError: true,
-        errorMsg: 'Invalid Credentials',
-        username: '',
-        password: '',
-      })
+        errorMsg: "Invalid Credentials",
+        username: "",
+        password: "",
+      });
     }
-  }
+  };
 
-    const renderError = () => {
-    const toggleForm = form.isError ? 'danger' : ''
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
     if (form.isError) {
       return (
-        <button type='submit' className={toggleForm}>
+        <button type="submit" className={toggleForm}>
           {form.errorMsg}
         </button>
-      )
+      );
     } else {
-      return <button type='submit'>Sign In</button>
+      return <button type="submit">Sign In</button>;
     }
-  }
+  };
 
-  const { username, password } = form
+  const { username, password } = form;
 
-  
   return (
     <div>
       <Form onSubmit={onSignIn}>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Username"
-        className="mb-3"
-      >
-        <Form.Control
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Username"
+          className="mb-3"
+        >
+          <Form.Control
             type="text"
             placeholder=""
             name="username"
-            value={form.username}
+            value={username}
             onChange={handleChange}
-        />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password">
           <Form.Control
             type="password"
             placeholder="Password"
             name="password"
-            value={form.password}
+            value={password}
             onChange={handleChange}
           />
-      </FloatingLabel>
-      {renderError()}
+        </FloatingLabel>
+        {renderError()}
       </Form>
     </div>
   );
 }
 
 export default Login;
-
-
-
-
