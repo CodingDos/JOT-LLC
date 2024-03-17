5a88686c8988ec2c5d81c4769f44e53a3c6ee215
John Lopez <john.lopez3695@gmail.com>
2024-03-16 21:26:43 -0400

    Initial route path for App.jsx done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
-import './App.css'
-import Landing from '../pages/Landing.jsx'
-import Footer from '../components/Footer.jsx'
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import { useState, useEffect } from "react";
 
+import Landing from "../pages/Landing.jsx";
+import Counter from "../pages/Counter.jsx";
+import Category from "../pages/Category.jsx";
+import Data from "../pages/Data.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import Footer from "../components/Footer.jsx";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-    </Routes>
-    <Footer />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/data-page/:categoryID" element={<Data />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
