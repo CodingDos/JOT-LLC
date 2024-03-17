6bccc0fa790c1df299a79f13a345d3acba66fd0f
John Lopez <john.lopez3695@gmail.com>
2024-03-17 18:59:24 -0400

    Resolved merge conflict

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import Button from "react-bootstrap/Button"
-import { useLocation, Link } from "react-router-dom"
-import * as navcss from "../styles/Nav.css"
-
-=======
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { signOut } from "../services/users.js";
->>>>>>> d272296188524d5c43caae841d121d91299a2393
+import * as navcss from "../styles/Nav.css";
 
 function Hamburger() {
   let navigate = useNavigate();
@@ -41,31 +32,12 @@ function Hamburger() {
     }
   };
   return (
-<<<<<<< HEAD
-    <Navbar className="rootNav"expand="">
-        <Container>
-            <Navbar.Brand className ="logo"href="/"> Jot </Navbar.Brand>
-            <Nav className="ms-auto">
-                <Link to={buttonLink}>
-                    <Button className="">{buttonName}</Button>
-                </Link>
-            </Nav>
-                <Navbar.Toggle aria-controls="navbar" />
-                <Navbar.Collapse id="navbar">
-                    <Nav className="navbarList">
-                        <Nav.Link href="/" className="navbarListItems">Landing</Nav.Link>
-                        <Nav.Link href="category" className="navbarListItems">Category</Nav.Link>
-                        <Nav.Link href="About" className="navbarListItems">About</Nav.Link>
-                        <Nav.Link href="/" className="navbarListItems">Logout</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-        </Container>
-    </Navbar>    
-  )
-=======
-    <Navbar expand="">
+    <Navbar className="rootNav" expand="">
       <Container>
-        <Navbar.Brand href="/"> Jot </Navbar.Brand>
+        <Navbar.Brand className="logo" href="/">
+          {" "}
+          Jot{" "}
+        </Navbar.Brand>
         <Nav className="ms-auto">
           <Link to={buttonLink}>
             <Button className="">{buttonName}</Button>
@@ -73,10 +45,16 @@ function Hamburger() {
         </Nav>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar">
-          <Nav className="navList">
-            <Nav.Link href="/">Landing</Nav.Link>
-            <Nav.Link href="category">Category</Nav.Link>
-            <Nav.Link href="About">About</Nav.Link>
+          <Nav className="navbarList">
+            <Nav.Link href="/" className="navbarListItems">
+              Landing
+            </Nav.Link>
+            <Nav.Link href="category" className="navbarListItems">
+              Category
+            </Nav.Link>
+            <Nav.Link href="About" className="navbarListItems">
+              About
+            </Nav.Link>
             <Nav.Link onClick={handleLogout} href="/">
               Logout
             </Nav.Link>
@@ -85,7 +63,6 @@ function Hamburger() {
       </Container>
     </Navbar>
   );
->>>>>>> d272296188524d5c43caae841d121d91299a2393
 }
 
 export default Hamburger;
