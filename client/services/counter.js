64b7456826d02e7124563698e310a92cc8db3353
John Lopez <john.lopez3695@gmail.com>
2024-03-17 18:59:49 -0400

    Initial counter functionality

---
+++ client/services/counter.js
@@ -0,0 +1,44 @@
+import api from "./apiConfig";
+
+export const getCounters = async (categoryId) => {
+  try {
+    const response = await api.get(`/counters/${categoryId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const createCounter = async (categoryId, counterData) => {
+  try {
+    const response = await api.post(
+      `/counters/create/${categoryId}`,
+      counterData
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const editCounter = async (counterId, counterData) => {
+  try {
+    const response = await api.put(`/counters/${counterId}`, counterData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const deleteCounter = async (counterId) => {
+  try {
+    const response = await api.delete(`/counters/${counterId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
